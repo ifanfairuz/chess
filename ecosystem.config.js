c004a2a63857aaca264796d1bf62c79ce4f0c74b
Ifan Fairuz <ifanfairuz@gmail.com>
2022-11-04 11:01:19 +0700

    DONE

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "chess",
+      script: "./server.js",
+      instances: 1,
+      error_file: "./logs/<app name>-error.log",
+      log_file: "./logs/<app name>-log.log",
+      time: true,
+      autorestart: true,
+    },
+  ],
+};
