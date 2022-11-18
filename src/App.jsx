fe9ac3bcc3180fec9c0c351bc0270a21810c32e2
Ifan Fairuz <ifanfairuz@gmail.com>
2022-11-18 19:52:33 +0700

    add CvC

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,14 @@ function App() {
       <div className="container-control">
         {mode === "PvC" ? (
           <Fragment>
-            <h4>Computer Vs Computer</h4>
+            <h4>Player Vs Computer</h4>
             <button className="button" onClick={() => setMode("CvC")}>
               Switch
             </button>
           </Fragment>
         ) : (
           <Fragment>
-            <h4>Player Vs Computer</h4>
+            <h4>Computer Vs Computer</h4>
             <button className="button" onClick={() => setMode("PvC")}>
               Switch
             </button>
